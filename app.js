a553cbeed4d538540d487037ffb1f36d26034cb8
Paulin K. Domfang <pk.domfang@gmail.com>
2022-09-30 19:32:23 +0200

    Users and Authentification implementation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require('cors');
 require("dotenv").config();
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options('*', cors())
@@ -11,13 +14,15 @@ app.options('*', cors())
 //Middleware
 app.use(express.json());
 app.use(morgan("tiny"));
-
+app.use(authJwt());
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
+
 
 const api = process.env.API_URL;
 
